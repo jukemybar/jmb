59ad32ec042824b11f83c1a3393f9addf71b527e
tgdguni4 <nicolas.gumy@swisscom.com>
2014-08-25 16:55:26 +0200

    fix auto setup

--- server/main.js
+++ server/main.js
@@ -5,6 +5,7 @@ Meteor.startup(function() {
             playerId: 0,
         });
     }
+
     ServiceConfiguration.configurations.remove({
         $or: [{
             service: "facebook"
@@ -14,16 +15,27 @@ Meteor.startup(function() {
             service: "google"
         }]
     });
+
+    var url = window.location;
+    if (url.match(/^(http:)\/\/(.+\.)?(localhost)\./)) {
+        ServiceConfiguration.configurations.insert({
+            service: "google",
+            clientId: "683764162464-17kafe2t7q6ofpama9lrdv9q63u78cp6.apps.googleusercontent.com",
+            secret: "k2yy6BJorow_eD09RQr8Wb7_"
+        });
+    } else {
+        ServiceConfiguration.configurations.insert({
+            service: "google",
+            clientId: "80599452496-2qof1vreif0kfki7ugcbb74d918smko8.apps.googleusercontent.com",
+            secret: "YUqQ_fvAjBd3oDbLxrazWgXp"
+        });
+    }
     // Add Google configuration entry
     //jmb.meteor.com
     // ServiceConfiguration.configurations.insert({
     //   service: "google", clientId: "80599452496-2qof1vreif0kfki7ugcbb74d918smko8.apps.googleusercontent.com", secret: "YUqQ_fvAjBd3oDbLxrazWgXp"
     // });
-    ServiceConfiguration.configurations.insert({
-        service: "google",
-        clientId: "683764162464-17kafe2t7q6ofpama9lrdv9q63u78cp6.apps.googleusercontent.com",
-        secret: "k2yy6BJorow_eD09RQr8Wb7_"
-    });
+
     //localhost:4000
     // ServiceConfiguration.configurations.insert({
     //   service: "google", clientId: "80599452496-khm9fh2kk4uk7hd0v6048tpj79qjdorg.apps.googleusercontent.com", secret: "VdBhGwxwOJARmuXClP1zAoq2"
