62c4656fd02427d71f5aa349877683d7cee98655
tgdguni4 <nicolas.gumy@swisscom.com>
2014-08-25 16:49:31 +0200

    Fix account settings

--- server/accounts.js
+++ server/accounts.js
@@ -108,18 +108,3 @@ UserStatus.events.on("connectionLogin", function(fields) {
 });
 
 
-ServiceConfiguration.configurations.remove({
-  service: "google"
-});
-//jmb.meteor.com
-// ServiceConfiguration.configurations.insert({
-//   service: "google", clientId: "80599452496-2qof1vreif0kfki7ugcbb74d918smko8.apps.googleusercontent.com", secret: "YUqQ_fvAjBd3oDbLxrazWgXp"
-// });
-ServiceConfiguration.configurations.insert({
-  service: "google", clientId: "683764162464-17kafe2t7q6ofpama9lrdv9q63u78cp6.apps.googleusercontent.com", secret: "k2yy6BJorow_eD09RQr8Wb7_"
-});
-//localhost:4000
-// ServiceConfiguration.configurations.insert({
-//   service: "google", clientId: "80599452496-khm9fh2kk4uk7hd0v6048tpj79qjdorg.apps.googleusercontent.com", secret: "VdBhGwxwOJARmuXClP1zAoq2"
-// });
-
